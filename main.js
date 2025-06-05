734287382ea8543eae81d2a75e1cae80cbf2be00
Mansoor Barri <mansoor.eb.ak@gmail.com>
2025-06-05 08:44:50 +0100

    update json

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Display airport taxi charts in GeoFS, with a search feature for ICAO codes, fetching data from GitHub.
-// @author       Your Name
+// @author       MansoorBarri
 // @match        https://www.geo-fs.com/geofs.php?v=*
 // @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
